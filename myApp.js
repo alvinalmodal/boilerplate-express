426859262671285073b14b9c7170b38eca6eff2e
alvin almodal <almodalalvin@gmail.com>
2021-06-06 19:17:02 +0800

    modified app to include serving of static files in public folder

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 var express = require("express");
+var path = require("path");
 var app = express();
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   let absolutePath = __dirname + "/views/index.html";
