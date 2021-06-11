0cbd012a293beb5b3bc6ea74d0690751e3269677
alvin almodal <almodalalvin@gmail.com>
2021-06-11 09:23:53 +0800

    added mongoose and nodemon

--- myApp.js
+++ myApp.js
@@ -1,6 +1,22 @@
 require("dotenv").config();
 var express = require("express");
 const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.wqup4.mongodb.net/test?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("mongodb connected...");
+});
+
 var path = require("path");
 var app = express();
 
